969633644a85880fb59c8bb01e2463beb9e0d9d1
Thais Ribeiro <thaisribeiro.mesquita@gmail.com>
2019-06-21 03:10:11 -0300

    Inclusão de projetos JS

---
+++ calculadora-js/controller/calcController.js
@@ -0,0 +1,229 @@
+class CalcController {
+
+	constructor(){
+		this._operation = [];
+		this._locale = 'pt-br';
+		this._displayCalcEl = document.querySelector('#display');
+		this._dateEl = document.querySelector('#data');
+		this._timeEl = document.querySelector('#hora');
+		this._currentDate;
+		this.initialize();
+		this.initButtonsEvents();
+	}
+
+
+	initialize() {	
+		this.setDisplayDateTime();
+		setInterval(() => {
+			this.setDisplayDateTime();
+		}, 1000);
+
+		this.setLastNumberToDisplay();
+	}
+
+	clearAll(){
+		this._operation = [];
+		this.setLastNumberToDisplay();
+	}
+
+	clearEntry(){
+		this._operation.pop();
+		this.setLastNumberToDisplay();
+	}
+
+	setError(){
+		this.displayCalc = 'Error';
+	}
+
+	getLastOperation() {
+		return this._operation[this._operation.length-1];
+	}
+
+	setLastOperation(value) {
+		this._operation[this._operation.length-1] = value;
+	}
+
+	isOperator(value){
+		return (['+','-','*','/','%'].indexOf(value) > -1);
+	}
+
+	pushOperation(value){
+		this._operation.push(value);
+		if(this._operation.length > 3) {
+			this.calc();
+		}
+	}
+
+	calc() {
+
+		let last = '';
+
+		if(this._operation.length > 3 ){
+			last = this._operation.pop();
+		}
+	
+		let result = eval(this._operation.join(""));
+
+		if(last == '%'){
+			result = result /100;
+			this._operation = [result];
+
+		} else {			
+			this._operation = [result];
+
+			if(last) this._operation.push(last);
+		}
+
+	}
+
+	setLastNumberToDisplay() {
+		let lastNumber;
+		for(let i = this._operation.length - 1; i>=0; i--){
+			if(!this.isOperator(this._operation[i])) {
+				lastNumber = this._operation[i];
+				break;
+			}
+		}
+
+		if(!lastNumber) lastNumber = 0;
+		this.displayCalc = lastNumber;
+	}
+
+	addOperation(value) {
+		if (isNaN(this.getLastOperation())) {
+			
+			if (this.isOperator(value)) {
+				this.setLastOperation(value);
+
+			} else if(isNaN(value)){
+				//outra coisa
+				console.log("Outra coisa");
+			} else {
+				this.pushOperation(value);
+				this.setLastNumberToDisplay();
+			}
+
+		} else {
+			
+			if (this.isOperator(value)){
+				this.pushOperation(value);
+				this.setLastNumberToDisplay();
+			} else {
+				let newValue = this.getLastOperation().toString() + value.toString();
+				this.setLastOperation(parseInt(newValue));
+
+				this.setLastNumberToDisplay();
+			}
+
+			
+		}
+		console.log(this._operation);
+	}
+
+	execBtn(value) {
+
+		switch (value) {
+			case 'ac':
+				this.clearAll();
+				break;
+			case 'ce':
+				this.clearEntry();
+				break;
+			case 'porcento':
+				this.addOperation('%');
+				break;
+			case 'soma':
+				this.addOperation('+');
+				break;
+			case 'subtracao':
+				this.addOperation('-');
+				break;
+			case 'multiplicacao':
+				this.addOperation('*');
+				break;
+			case 'divisao':
+				this.addOperation('/');
+				break;
+			case 'ponto':
+				this.addOperation('.');
+				break;
+			case 'igual':
+				this.calc();
+				break;
+			case '0':
+			case '1': 
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				this.addOperation(parseInt(value));
+				break;
+			default:
+			this.setError();
+				break;
+
+		}
+	}
+
+	addEventListenerAll(element, events, fn) {
+		events.split(' ').forEach(event =>{
+			element.addEventListener(event,fn, false);
+		});
+	}
+
+	initButtonsEvents() {
+		let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+		
+		buttons.forEach((btn, index) => {
+			this.addEventListenerAll(btn,'click drag', e => {
+				let textBtn = btn.className.baseVal.replace("btn-","");
+				console.log(textBtn);
+				this.execBtn(textBtn);
+			});
+			this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+				btn.style.cursor = "pointer";
+			});
+		});
+	}
+
+	setDisplayDateTime() {
+		this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+		this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+	}
+
+	get displayTime() {
+		return this._timeEl.innerHTML;
+	}
+
+	set displayTime(value) {
+		this._dateEl.innerHTML = value;
+	}
+
+	get displayDate() {
+		return this._dateEl.innerHTML;
+	}
+
+	set displayDate(value) {
+		this._timeEl.innerHTML = value;
+	}
+
+	get displayCalc() {
+		return this._displayCalcEl.innerHTML;
+	}
+
+	set displayCalc(value) {
+		this._displayCalcEl.innerHTML = value;
+	}
+
+	get currentDate() {
+		return new Date();
+	}
+
+	set currentDate(value) {
+		this._currentDate = value;
+	}
+}
---
+++ multiroom-js/app.js
@@ -0,0 +1,43 @@
+var app = require('./config/server');
+
+var server = app.listen(80, function(){
+	console.log('Servidor online');
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+io.on('connection', function(socket){
+	console.log('Usuário conectou');
+
+	socket.on('disconnect', function(){
+		console.log('Usuário desconectou');
+	});
+
+	socket.on('msgParaServidor', function(data){
+
+		socket.emit(
+			'msgParaCliente', 
+			{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+		socket.broadcast.emit(
+			'msgParaCliente', 
+			{apelido: data.apelido, mensagem: data.mensagem}
+		);
+
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit(
+				'participantesParaCliente', 
+				{apelido: data.apelido}
+			);
+
+			socket.broadcast.emit(
+				'participantesParaCliente', 
+				{apelido: data.apelido}
+			);
+		}
+	});
+
+});
